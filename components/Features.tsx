e07b5936a28c4a49cbbaf0270efb32fa8e6d08b7
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-08 13:05:47 -0500

    created Features component

---
+++ components/Features.tsx
@@ -0,0 +1,42 @@
+import React, { ElementType } from "react";
+
+import { Box, HStack, Icon, Text, StackProps } from "@chakra-ui/react";
+
+// Icons components
+import {
+  MoneyBackGuaranteeIcon,
+  HassleFreeIcon,
+  MonthlySubscriptionIcon,
+} from "../src/icons/Icon";
+
+interface FeatureProps extends StackProps {
+  icon: ElementType;
+}
+
+function Feature(props: FeatureProps) {
+  const { icon, children, ...rest } = props;
+  return (
+    <HStack {...rest} spacing="24px">
+      <Icon as={icon} boxSize="48px" />
+      <Text textAlign="left" fontSize="18px" fontWeight="700">
+        {children}
+      </Text>
+    </HStack>
+  );
+}
+
+export default function Features() {
+  return (
+    <Box maxW="1024px" m="auto" pt="60px" pb="32px">
+      <HStack px="48px" spacing="20px">
+        <Feature icon={MoneyBackGuaranteeIcon}>
+          30 Days money back Guarantee
+        </Feature>
+        <Feature icon={HassleFreeIcon}>No setup fess 100% hassle-free</Feature>
+        <Feature icon={MonthlySubscriptionIcon}>
+          No Monthly subscription Pay once and for all
+        </Feature>
+      </HStack>
+    </Box>
+  );
+}
