04d8e6a94ca74ce183c49769a4394520212f37cc
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-08 11:10:46 -0500

    completed Pricing component

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,20 +1,72 @@
-import React from "react"
-import {Button, Box, Flex, Heading, Text} from "@chakra-ui/react"
+import React from "react";
+import {
+  Button,
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Stack,
+  HStack,
+  Icon,
+  StackProps,
+} from "@chakra-ui/react";
+import CheckIcon from "../src/icons/Icon";
+
+// ListItem component
+export const ListItem = (props: StackProps) => {
+  // Destructure children from props
+  // Children will be rendered in between the Text elements
+  const { children, ...rest } = props;
+
+  return (
+    <HStack as="li" spacing="20px" {...rest}>
+      <Icon as={CheckIcon} w="22px" h="22px" />
+      <Text>{children}</Text>
+    </HStack>
+  );
+};
 
 export default function Pricing() {
-    return(
-        <Box>
-            <Flex>
-                <Box bg='#F0EAFB'p='60px'>
-                    <Text fontSize='24px' fontWeight='800'>Premium PRO</Text>
-                    <Heading as='h3' fontSize='60px' mt='16px'>$329</Heading>
-                    <Text color='121923' fontSize='18px' fontWeight='500' mt='8px'>billed just once</Text>
-                    <Button colorScheme='purple' size='lg' w='282px' mt='24px'>Get Started</Button>
-                </Box>
-                <Box>
-
-                </Box>
-            </Flex>
+  return (
+    <Box
+      maxW="994px"
+      margin="auto"
+      mt="-256px"
+      borderRadius="12px"
+      overflow="hidden"
+      boxShadow="0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)"
+      textAlign="center"
+    >
+      <Flex>
+        {/*Right Section */}
+        <Box bg="#F0EAFB" p="60px">
+          <Text fontSize="24px" fontWeight="800">
+            Premium PRO
+          </Text>
+          <Heading as="h3" fontSize="60px" mt="16px">
+            $329
+          </Heading>
+          <Text color="121923" fontSize="18px" fontWeight="500" mt="8px">
+            billed just once
+          </Text>
+          <Button colorScheme="purple" size="lg" w="282px" mt="24px">
+            Get Started
+          </Button>
         </Box>
-    )
+        {/*Left Section */}
+        <Box p="60px" fontSize="18px" bg="white">
+          <Text textAlign="left">
+            Access these features when you get this pricing package for your
+            business
+          </Text>
+          <Stack as="ul" spacing="20px" pt="24px">
+            <ListItem>International calling and messaging API</ListItem>
+            <ListItem>Additional phone numbers</ListItem>
+            <ListItem>Automated messages via Zapier</ListItem>
+            <ListItem>24/7 Support and Consulting</ListItem>
+          </Stack>
+        </Box>
+      </Flex>
+    </Box>
+  );
 }
