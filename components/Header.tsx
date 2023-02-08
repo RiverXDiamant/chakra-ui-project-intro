7b93a4fa0eeaaea79d30c08e92bfd88760c6f21a
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-08 09:40:39 -0500

    added Header component

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import {Box, Heading, Text} from "@chakra-ui/react"
+
+export default function Header() {
+    return (
+        <Box>
+            <Heading>Simple pricing for your business</Heading>
+            <Text>Plans that are carefully crafted to suit your business.</Text>
+        </Box>
+    )
+}
